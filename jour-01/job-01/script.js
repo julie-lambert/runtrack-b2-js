c6526c36531b70a914a2da0ce75dc5c64994a9bf
julie-lambert <julie.lambert@laplateforme.io>
2023-09-06 15:42:46 +0200

    job 01

---
+++ jour-01/job-01/script.js
@@ -0,0 +1,32 @@
+function myCount(string){
+    let i = 0
+    while (string[i]!== undefined){
+        i++;
+    }
+    return i
+}
+
+function myUpperCase(string){
+
+    const min = "abcdefghijklmnopqrstuvwxyz"
+    const maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let myString= ""
+
+    for(let i = 0; i < myCount(string); i++){
+        for(let j = 0; j < myCount(min); j++){
+            if(string[i] === min[j]){
+                myString += maj[j]
+                break
+            }
+            else if(j === 25){
+                myString += string[i]
+                break
+            }
+        }
+
+    }
+    return myString
+
+}
+
+console.log(myUpperCase("Hello World"))
